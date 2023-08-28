d1a98469ed366f8e3c9eae218655cf764dd13a78
deanbirnie <deanicdl@gmail.com>
2023-08-27 17:19:24 -0700

    Task 4 - Changed format of Puppet file to edit specific lines in ssh config file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,13 @@
 # This puppet script sets up access to a server using only a public key and
 # disables password authentication
-file { '~/.ssh/config':
-  ensure => file,
-  owner  => 'root',
-  mode   => '0600',
-  content => "\
-Host *
-    IdentityFile ~/.ssh/school
-    PreferredAuthentications publickey
-    PasswordAuthentication no\n",
+file_line { 'Password Authentication No':
+  ensure => 'present',
+  path   => '/~/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Identity File school':
+  ensure => 'present',
+  path   => '/~/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
 }
