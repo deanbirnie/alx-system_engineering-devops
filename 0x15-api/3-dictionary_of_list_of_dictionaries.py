ac5cfe02306e66bd640544daca742e1d4f3ddc90
deanbirnie <deanicdl@gmail.com>
2023-09-27 13:57:29 -0700

    Task 3 - Export all data in JSON format, record all tasks from all employees

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+This script fetches all tasks for all employees and records them in a
+structured JSON file, 'todo_all_employees.json'.
+"""
+
+import requests
+import json
+
+USERS_URL = "https://jsonplaceholder.typicode.com/users"
+TODOS_URL = "https://jsonplaceholder.typicode.com/todos"
+
+
+def get_data(url):
+    """
+    Fetches data from the specified URL and returns it as a JSON object.
+    If the request fails, it raises an HTTPError.
+    """
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        response.raise_for_status()
+
+
+def main():
+    """
+    Fetches all users and their tasks, structures them according to
+    requirements, and writes them to 'todo_all_employees.json' in JSON format.
+    """
+    users = get_data(USERS_URL)
+
+    all_users_tasks = {}
+
+    for user in users:
+        user_id = user.get('id')
+        username = user.get('username')
+
+        user_todos = get_data(f"{TODOS_URL}?userId={user_id}")
+
+        user_tasks = [
+            {"username": username, "task": todo.get(
+                'title'
+            ), "completed": todo.get('completed')}
+            for todo in user_todos
+        ]
+
+        all_users_tasks[str(user_id)] = user_tasks
+
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump(all_users_tasks, f)
+
+
+if __name__ == "__main__":
+    main()
