9a88d8ecfabc653fe13e7e51fd0374a41ed7bf7d
deanbirnie <deanicdl@gmail.com>
2023-10-10 14:32:51 -0700

    Task 2 - recursive function to return list of all hot posts for a given subreddit using Reddit API

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Module for querying Reddit API and separating pages of responses
+"""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """
+    """
+    headers = {
+        'User-Agent': 'script to scrape post titles by u/deano_southafrican'
+        }
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    if after:
+        url += "?after={}".format(after)
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        return None
+
+    data = response.json()['data']
+    hot_list += [post['data']['title'] for post in data['children']]
+
+    if data['after']:
+        return recurse(subreddit, hot_list, data['after'])
+
+    return hot_list
