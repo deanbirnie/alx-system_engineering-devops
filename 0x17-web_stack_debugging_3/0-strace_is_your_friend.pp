a65b3352ae9156f5d5fc9ed3472ca9259797d53a
deanbirnie <deanicdl@gmail.com>
2023-10-12 12:28:08 -0700

    Task 0 - Corrected puppet formatting

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fixes an issue with a specific Wordpress installtion
 
 exec {'wp-settings-fix':
-     command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-     path    => '/usr/local/bin/:/bin/'
-     }
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+  }
